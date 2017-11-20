8468ca64a200fdd65805e1f0d0b266bb34259ab6
msantam2 <msantam2@gmail.com>
2017-11-19 21:57:57 -0800

    Add LovedOne upon accepting request.

--- src/components/ReceivedRequestsList.js
+++ src/components/ReceivedRequestsList.js
@@ -10,16 +10,46 @@ class ReceivedRequestsList extends React.Component {
     super(props);
     this.renderItem = this.renderItem.bind(this);
     this.fetchPendingRequests = this.fetchPendingRequests.bind(this);
-    this.onRespond = this.onRespond.bind(this);
+    this.onAccept = this.onAccept.bind(this);
+    this.onDecline = this.onDecline.bind(this);
+    this.addLovedOne = this.addLovedOne.bind(this);
   }
 
   componentDidMount() { // LATER: setup a listener to new loved one requests
     this.fetchPendingRequests();
   }
 
-  onRespond(request, status) {
-    request.ref.set({ status }, { merge: true });
+  onAccept(request) {
+    request.ref.set({ status: 'accepted' }, { merge: true });
     this.props.removeReceivedRequest(request);
+    this.addLovedOne(request);
+  }
+
+  onDecline(request) {
+    request.ref.set({ status: 'declined' }, { merge: true });
+    this.props.removeReceivedRequest(request);
+  }
+
+  addLovedOne(request) { // Not static yet. may use 'this' in promise handlers
+    const id = request.data().requesterId;
+    const email = request.data().requesterEmail;
+    const lovedOneDoc = {
+      id,
+      email,
+      createdAt: new Date()
+    };
+
+    const user = Firebase.auth().currentUser; // LATER: change to props
+    const db = Firebase.firestore();
+    const lovedOnesRef = db.collection(`users/${user.uid}/lovedOnes`);
+
+    lovedOnesRef.add(lovedOneDoc)
+      .then(() => {
+        // success. doing nothing OK for now.
+      })
+      .catch(() => {
+        // error. doing nothing OK for now.
+      });
   }
 
   fetchPendingRequests() {
@@ -47,8 +77,8 @@ class ReceivedRequestsList extends React.Component {
   renderItem = ({ item }) => (
     <ReceivedRequest
       requestLabel={item.data().requesterEmail}
-      onAccept={() => this.onRespond(item, 'accepted')}
-      onDecline={() => this.onRespond(item, 'declined')}
+      onAccept={() => this.onAccept(item)}
+      onDecline={() => this.onDecline(item)}
     />
   )
 
