8bbf6e27b7d4b70af2cabb618d3a9873d7e41a83
msantam2 <msantam2@gmail.com>
2018-01-23 23:42:08 -0800

    Successfully upload an image to Firebase Storage from the client. Make use of 'react-native-fetch-blob' and 'react-native-uuid-generator' to create a Blob from the image uri, and then save it to Storage with a unique UUID.

--- src/components/raindrop/SendRaindrop.js
+++ src/components/raindrop/SendRaindrop.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import Firebase from 'firebase';
+import UUIDGenerator from 'react-native-uuid-generator';
+import RNFetchBlob from 'react-native-fetch-blob';
+
 import Sender from '../common';
 import { changeSendRaindropStatus } from '../../actions';
 
@@ -20,9 +23,35 @@ class SendRaindrop extends React.Component {
     let { user } = this.props;
     user = user || Firebase.auth().currentUser;
     const { imageUri, foundRaindropRecipient } = this.props;
+
+    UUIDGenerator.getRandomUUID().then((uuid) => {
+      const imageUUID = uuid;
+      const imageRef = Firebase.storage().ref(`images/${imageUUID}`);
+
+      const Blob = RNFetchBlob.polyfill.Blob;
+      window.Blob = Blob;
+      window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
+      const mime = 'image/jpeg';
+
+      RNFetchBlob.fs.readFile(imageUri, 'base64')
+        .then((data) => {
+          Blob.build(data, { type: `${mime};BASE64` })
+            .then((blob) => {
+              imageRef.put(blob, { contentType: mime })
+                .then(() => {
+                  imageRef.getDownloadURL()
+                    .then((downloadURL) => {
+                      // console.log(downloadURL);
+                    });
+                });
+            });
+        });
+    });
   }
 
   render() {
+    this.sendRaindrop();
+
     return (
       <Text>
         Hello from SendRaindrop
